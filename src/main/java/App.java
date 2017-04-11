d0767f8eae9de169e1aa1627ef97e2fe14e3c19e
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-11 10:01:47 -0700

    add index.vtl and routes for index and start game

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,24 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
+      model.put("topScores", Game.getTopScores());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/start-game", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String playerName = request.queryParams("name");
+      Game newGame = new Game(playerName);
+      newGame.save();
+      request.session().attribute("currentGame", newGame);
+      request.redirect("/level-1");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/level-1", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/level-1.vtl");
+      request.session().attribute("currentGame");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
