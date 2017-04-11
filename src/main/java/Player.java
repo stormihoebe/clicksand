a6dd0a199490f86249914baba79080484767d0b8
Gloria Friesen and Ryan McAlpin <gloria@>
2017-04-11 09:38:41 -0700

    add save, find, equals, and all methods to Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -19,4 +19,45 @@ public class Player {
     return name;
   }
 
+  public int getId() {
+    return id;
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO players (name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Player> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM players";
+      return con.createQuery(sql)
+        .executeAndFetch(Player.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherPlayer) {
+    if (!(otherPlayer instanceof Player)) {
+      return false;
+    } else {
+      Player newPlayer = (Player) otherPlayer;
+      return this.getName().equals(newPlayer.getName()) && this.getId() == newPlayer.getId();
+    }
+  }
+
+  public static Player find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM players WHERE id = :id";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Player.class);
+    }
+  }
+
 }
