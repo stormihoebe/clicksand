1c7071721d8cbc6cc8b84691a92047df6e959926
Riley Watts <rileywatts.yes@gmail.com>
2017-04-12 09:12:40 -0700

    edit level.vtl, add level-success vtl, refactor app.java routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,10 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
+
+
 public class App {
+
   public static void main(String[] args) {
     // Use this setting only for development
     externalStaticFileLocation(String.format("%s/src/main/resources/public", System.getProperty("user.dir")));
@@ -21,25 +24,27 @@ public class App {
 
     post("/start-game", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      // String playerName = request.queryParams("name");
-      // Game newGame = new Game(playerName);
-      // newGame.save();
-      // request.session().attribute("currentGame", newGame);
-      // request.redirect("/level-1");
+      String playerName = request.queryParams("name");
+      Game newGame = new Game(playerName);
+      newGame.save();
+      request.session().attribute("game", newGame);
+      response.redirect("/level");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/level-success", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Game curentGame = Game.find();//how to get current game Object
+      currentGame.nextLevel();//adds 1 to current level id;
+      request.session().attribute("game");
+      model.put("template", "templates/level-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/level", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/level.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    post("/level2", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/level2.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/level-1", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/level-1.vtl");
-      request.session().attribute("currentGame");
+      request.session().attribute("game");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -1,3 +1,6 @@
+///Twenty Second Timer
+
+
 function startTimer(duration, display) {
     var timer = duration, seconds;
     setInterval(function () {
@@ -12,6 +15,24 @@ function startTimer(duration, display) {
 }
   jQuery(function ($) {
       var twentySeconds = 20,
-          display = $('#time');
+          display = $('#time20');
+      startTimer(twentySeconds, display);
+});
+///Thirty Second Timer
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.text("00:" + seconds);
+
+        if (--timer < 0) {
+            timer = 0;
+        }
+    }, 1000);
+}
+  jQuery(function ($) {
+      var twentySeconds = 50,
+          display = $('#time30');
       startTimer(twentySeconds, display);
 });
