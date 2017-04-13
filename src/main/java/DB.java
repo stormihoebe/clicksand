ebb12de1bb459ad8b385cede346b27c609064cfb
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-13 14:22:05 -0700

    add changes to DB.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,9 @@
 import org.sql2o.*;
 
+// public class DB {
+//   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/clickbait_game", null, null);
+// }
+
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/clickbait_game", null, null);
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-235-181-120.compute-1.amazonaws.com:5432/dfdejpi5p1f280","jsoikojsmkhips", "290c72f91a0c81551278f1b131ac7c9623a725e6b8bc0ae8598b8b94305ad1b2");
 }
